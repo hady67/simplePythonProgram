ca350ae6d06582f03cf5b781293622fb62a727f4
Hardikk Kamboj <64458111+hady67@users.noreply.github.com>
2020-10-02 16:18:15 +0530

    added some useful comments
    wrote some comments which can help people to identify the usage of the given lines !

--- Calculator.py
+++ Calculator.py
@@ -1,18 +1,19 @@
 def calc(choice, first_num, second_num):
 # Python does not have switch case
+#this is a function that we used to call calc 
 # There are other ways to implement this, dictionary mapping for example, but I started off with this
     if(choice=='addition' or choice=='add'):
-        return first_num+second_num
+        return first_num+second_num     #this  returns addition of the two numbers the user added 
     elif (choice=='subtraction' or choice=='subtract'):
-        return first_num-second_num
+        return first_num-second_num    #this  returns subtraction of the two numbers the user added 
     elif(choice=='multiplication' or choice=='multiply'):
-        return first_num*second_num
+        return first_num*second_num    #this  returns multiplication of the two numbers the user added 
     elif (choice=='division' or choice=='divide'):
-        return first_num/second_num
+        return first_num/second_num    #this  returns division of the two numbers the user added 
 
 print("This is the Basic Calculator Program")
 start=input("Do you wish to start? Type 1 for YES and 0 for NO: ")
-st=int(start)
+st=int(start) #entering the yes or no option
 if(st!=1 and st!=0):
     print("I'm going to take that as a YES")
 while (st==1):
@@ -27,7 +28,7 @@ while (st==1):
     choice=choice.lower()
     num2=input("Enter the second number: ")
     second_num=int(num2)
-    answer=calc(first_num, choice, second_num)
+    answer=calc(first_num, choice, second_num) #here we are calling the calc function to do our task !
     a=int(answer)
     print("The answer is "+str(a))
     continue_=input("Do you want to continue? Press 1 for YES and 0 for NO: ")
